e8d45ade1e1552f3c6044ad5d492845d099a2461
Mirea Klee <mireaklee@Mireas-MacBook-Pro.local>
2022-10-28 15:35:03 -0400

    reset language to basic

--- live-lang/lang.js
+++ live-lang/lang.js
@@ -1,26 +1,17 @@
 
 var audioCtx;
-var osc = [];
-var timings = [];
+var osc;
+var timings;
 var liveCodeState = [];
 const playButton = document.querySelector('button');
-var globalGain;
 
-function initAudio(lines) {
-
-    //add for loop to create all the oscillators
-    let i = 0;
-    while (  i  <  lines ){
-        let o = audioCtx.createOscillator();
-        let t = audioCtx.createGain();
-        t.gain.value = 0;
-        o.connect(t).connect(globalGain);
-        o.start();
-
-        osc[i] = o;
-        timings[i] = t;
-    }
-    
+function initAudio() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)
+    osc = audioCtx.createOscillator();
+    timings = audioCtx.createGain();
+    timings.gain.value = 0;
+    osc.connect(timings).connect(audioCtx.destination);
+    osc.start();
     scheduleAudio()
 }
 
@@ -33,7 +24,6 @@ function scheduleAudio() {
         timings.gain.setTargetAtTime(0, audioCtx.currentTime + timeElapsedSecs, 0.01)
         timeElapsedSecs += 0.2; //rest between notes
     });
-
     setTimeout(scheduleAudio, timeElapsedSecs * 1000);
 }
 
@@ -43,27 +33,22 @@ function parseCode(code) {
     //how could we allow for two lines that play at the same time?
     //what if we want variables?
     //how does this parsing technique limit us?
-    let notes;
-    let phrase = code.split("//")
-    for (let p in phrase){
-        notes.push(phrase[p].split(" ").map(note => {
-            noteData = note.split("@");
-            return   {"length" : eval(noteData[0]), //the 'eval' function allows us to write js code in our live coding language
-                    "pitch" : eval(noteData[1])};
-                    //what other things should be controlled? osc type? synthesis technique?
-        }))
-    }
+    let notes = code.split(" ");
 
-    
     //notice this will fail if the input is not correct
     //how could you handle this? allow some flexibility in the grammar? fail gracefully?
     //ideally (probably), the music does not stop
+    notes = notes.map(note => {
+        noteData = note.split("@");
+        return   {"length" : eval(noteData[0]), //the 'eval' function allows us to write js code in our live coding language
+                "pitch" : eval(noteData[1])};
+                //what other things should be controlled? osc type? synthesis technique?
+    });
     return notes;
 }
 
 function genAudio(data) {
     liveCodeState = data;
-    initAudio(data.length);
 }
 
 function reevaluate() {
@@ -75,10 +60,7 @@ function reevaluate() {
 playButton.addEventListener('click', function () {
 
     if (!audioCtx) {
-        audioCtx = new AudioContext();
-        globalGain = audioCtx.createGain();
-        globalGain.connect(audioCtx.destination);
-        
+        initAudio();
     }
 
     reevaluate();
